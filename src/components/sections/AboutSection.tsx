9fc07914426d81bd26574c72e76ed1e793262127
bom0320 <s24040@gsm.hs.kr>
2026-01-15 17:28:41 +0900

    feat: about basic structure

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,7 +1,29 @@
+"use client";
+
 export default function AboutPage() {
   return (
-    <section id="about" className="about">
-      <div className="about_inner"></div>
+    <section id="about" className="about-hero">
+      <div className="about-hero__inner">
+        <h2 className="about-hero__title">ABOUT ME</h2>
+
+        <p className="about-hero__desc">
+          FrontEnd 개발자 김봄입니다. 사용자가 즐길 수 있는 직관적이고의미 있는
+          경험을 만드는 것을 목표로 합니다. FrontEnd 개발자 김봄입니다. 사용자가
+          즐길 수 있는 직관적이고의미 있는
+        </p>
+
+        <div className="about-hero__cta">
+          <a className="about-hero__btn about-hero__btn--primary" href="#">
+            <span>Go Resume</span>
+            <span className="about-hero__icon">→</span>
+          </a>
+
+          <a className="about-hero__btn" href="#">
+            <span>Go Github</span>
+            <span className="about-hero__icon">→</span>
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
