80c494d359eac830d45af29ef7c52106c2ac516a
bom0320 <bom0320.dev@gmail.com>
2026-05-29 08:56:54 +0900

    refactor: organize capability animations by scene

--- src/animations/capability/index.ts
+++ src/animations/capability/index.ts
@@ -1,6 +1,4 @@
-export { default as CapabilityIntroAnimation } from "./capabilityIntro.animation";
-export { default as CapabilityIntroProofAnimation } from "./capabilityIntroProof.animation";
-export { default as StructureCapabilityAnimation } from "./structureCapability.animation";
-export { default as AICapabilityAnimation } from "./aiCapability.animation";
-export { default as VisualCapabilityAnimation } from "./visualCapability.animation";
-export { default as CapabilityNavigatorAnimation } from "./capabilityNavigator.animation";
+export * from "./intro";
+export * from "./experience";
+export * from "./navigator";
+export * from "./closing";
