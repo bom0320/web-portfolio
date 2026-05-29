2b278e792bfd60e06e7fb09f1c77661658f7facb
bom0320 <bom0320.dev@gmail.com>
2026-05-29 09:25:05 +0900

    feat: capabilityClosingScene

---
+++ src/components/features/capability/closing/CapabilityClosingScrollCue.tsx
@@ -0,0 +1,8 @@
+export default function CapabilityClosingScrollCue() {
+  return (
+    <div className="capability-closing__scroll">
+      <span className="capability-closing__scroll-line" />
+      <span className="capability-closing__scroll-text">SCROLL TO CONNECT</span>
+    </div>
+  );
+}
--- src/components/scenes/capability/CapabilityClosingScene.tsx
+++ src/components/scenes/capability/CapabilityClosingScene.tsx
@@ -1,23 +1,15 @@
-"use client";
+import {
+  CapabilityClosingScrollCue,
+  CapabilityClosingStatement,
+} from "@/components/features/capability";
 
 export default function CapabilityClosingScene() {
   return (
-    <section className="capability-closing-scene">
-      <div className="capability-closing-scene__inner">
-        <h2 className="capability-closing-scene__title">
-          Let&apos;s build
-          <br />
-          better experiences.
-        </h2>
-
-        <p className="capability-closing-scene__desc">
-          흐름을 설계하고,
-          <br />
-          구조를 정리하고,
-          <br />
-          움직임에 이유를 담으면서.
-        </p>
+    <section className="capability-closing js-capability-closing">
+      <div className="capability-closing__inner">
+        <CapabilityClosingStatement />
       </div>
+      <CapabilityClosingScrollCue />
     </section>
   );
 }
