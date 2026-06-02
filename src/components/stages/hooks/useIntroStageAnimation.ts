5b51ede79f4019471f5bd6093677ea09541c3902
bom0320 <bom0320.dev@gmail.com>
2026-06-02 15:03:11 +0900

    fix: IntroStageAnimation

--- src/components/stages/hooks/useIntroStageAnimation.ts
+++ src/components/stages/hooks/useIntroStageAnimation.ts
@@ -6,7 +6,11 @@ import gsap from "gsap";
 import { AboutSceneAnimation } from "@/animations/about";
 import HeroToLifeAnimation from "@/animations/transitions/heroToLife";
 import LifeToAboutAnimation from "@/animations/transitions/lifeToAbout";
-import { createScrollTrigger, refreshScrollTrigger } from "@/lib/gsap";
+import {
+  createScrollTrigger,
+  refreshScrollTrigger,
+  type ScrollTriggerInstance,
+} from "@/lib/gsap";
 
 import { INTRO_STAGE_SCROLL_CONFIG, INTRO_STAGE_SELECTORS } from "../constants";
 
@@ -15,67 +19,76 @@ const clampProgress = (progress: number) => gsap.utils.clamp(0, 1, progress);
 export function useIntroStageAnimation() {
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      const heroToLifeController = HeroToLifeAnimation.create();
-      const lifeToAboutController = LifeToAboutAnimation.create();
-      const aboutSceneController = AboutSceneAnimation.create();
+      const controllers = {
+        heroToLife: HeroToLifeAnimation.create(),
+        lifeToAbout: LifeToAboutAnimation.create(),
+        aboutScene: AboutSceneAnimation.create(),
+      };
 
-      heroToLifeController.setProgress(0);
-      lifeToAboutController.setProgress(0);
-      aboutSceneController.setProgress(0);
+      controllers.heroToLife.setProgress(0);
+      controllers.lifeToAbout.setProgress(0);
+      controllers.aboutScene.setProgress(0);
 
-      const heroToLifeTrigger = createScrollTrigger({
-        trigger: INTRO_STAGE_SELECTORS.root,
-        start: INTRO_STAGE_SCROLL_CONFIG.heroToLife.start,
-        end: () =>
-          `+=${
-            window.innerHeight *
-            INTRO_STAGE_SCROLL_CONFIG.heroToLife.endMultiplier
-          }`,
-        scrub: INTRO_STAGE_SCROLL_CONFIG.heroToLife.scrub,
-        onUpdate: (self) => {
-          heroToLifeController.setProgress(self.progress);
-        },
-      });
+      const triggers: ScrollTriggerInstance[] = [];
 
-      const lifeToAboutTrigger = createScrollTrigger({
-        trigger: INTRO_STAGE_SELECTORS.root,
-        start: () =>
-          `top+=${
-            window.innerHeight *
-            INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.startOffsetMultiplier
-          } top`,
-        end: () =>
-          `+=${
-            window.innerHeight *
-            INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.endMultiplier
-          }`,
-        scrub: INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.scrub,
-        onUpdate: (self) => {
-          const progress = self.progress;
+      triggers.push(
+        createScrollTrigger({
+          trigger: INTRO_STAGE_SELECTORS.root,
+          start: INTRO_STAGE_SCROLL_CONFIG.heroToLife.start,
+          end: () =>
+            `+=${
+              window.innerHeight *
+              INTRO_STAGE_SCROLL_CONFIG.heroToLife.endMultiplier
+            }`,
+          scrub: INTRO_STAGE_SCROLL_CONFIG.heroToLife.scrub,
+          onUpdate: (self) => {
+            controllers.heroToLife.setProgress(self.progress);
+          },
+        })
+      );
 
-          const enterProgress = clampProgress(
-            progress / INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.enterRatio
-          );
+      triggers.push(
+        createScrollTrigger({
+          trigger: INTRO_STAGE_SELECTORS.root,
+          start: () =>
+            `top+=${
+              window.innerHeight *
+              INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.startOffsetMultiplier
+            } top`,
+          end: () =>
+            `+=${
+              window.innerHeight *
+              INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.endMultiplier
+            }`,
+          scrub: INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.scrub,
+          onUpdate: (self) => {
+            const progress = self.progress;
 
-          const sceneProgress = clampProgress(
-            (progress - INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.enterRatio) /
-              INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.sceneRatio
-          );
+            const enterProgress = clampProgress(
+              progress / INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.enterRatio
+            );
 
-          lifeToAboutController.setProgress(enterProgress);
-          aboutSceneController.setProgress(sceneProgress);
-        },
-      });
+            const sceneProgress = clampProgress(
+              (progress - INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.enterRatio) /
+                INTRO_STAGE_SCROLL_CONFIG.lifeToAbout.sceneRatio
+            );
+
+            controllers.lifeToAbout.setProgress(enterProgress);
+            controllers.aboutScene.setProgress(sceneProgress);
+          },
+        })
+      );
 
       refreshScrollTrigger();
 
       return () => {
-        heroToLifeTrigger.kill();
-        lifeToAboutTrigger.kill();
+        triggers.forEach((trigger) => {
+          trigger.kill();
+        });
 
-        heroToLifeController.destroy();
-        lifeToAboutController.destroy();
-        aboutSceneController.destroy();
+        Object.values(controllers).forEach((controller) => {
+          controller.destroy();
+        });
       };
     });
 
