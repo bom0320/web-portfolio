b07e619e798719ff8a26eb248e9ced5bdbd52862
bom0320 <bom0320.dev@gmail.com>
2026-06-09 11:14:16 +0900

    feat: add capability detail image lightbox

--- src/components/features/capability/navigator/detail/index.ts
+++ src/components/features/capability/navigator/detail/index.ts
@@ -1,2 +1,3 @@
 export { default as CapabilityDetailHero } from "./CapabilityDetailHero";
 export { default as CapabilityDetailGallery } from "./CapabilityDetailGallery";
+export { default as CapabilityDetailActions } from "./CapabilityDetailActions";
