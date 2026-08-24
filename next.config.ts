a1a7ec10ecf4ae69d7559fbe4a77675109ba1b64
bom0320 <bom0320.dev@gmail.com>
2026-08-24 17:58:46 +0900

    feat: add MDX support for project case studies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+
   sassOptions: {
     silenceDeprecations: ["import"],
   },
@@ -20,4 +23,8 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
